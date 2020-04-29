5675aa07e4d5dfac8d372da691fcc2528a41db01
Alex <Alex@Rojo1>
2020-04-29 19:03:19 +0200

    v2.0

--- src/database/Database.java
+++ src/database/Database.java
@@ -6,6 +6,8 @@
 package database;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.Scanner;
 import javax.swing.JOptionPane;
 import static javax.swing.JOptionPane.showInputDialog;
 
@@ -19,7 +21,13 @@ public class Database {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+    Scanner teclado = new Scanner (System.in);
     boolean flag = false;
+    String nome, tipo, fich;
+    double telefono;
+    int perso;
+    int contador, cod_cli, cod_esc;
+    ArrayList <Integer> codcliente = new ArrayList<Integer>();
     int seleccion = 99;
     ResultSet rs;
     
@@ -37,82 +45,96 @@ public class Database {
             System.out.println("Conectado á base de datos");
 
 
-            while (!flag){
+            while(!flag){
                 switch (Menu()){
                     case 1:
-                        stm=con.createStatement();
-                        stm.executeUpdate("CREATE TABLE CLIENTES ("
-                                + "COD_CLIENTE NUMBER(5) PRIMARY KEY,"
-                                + "NOMBRE VARCHAR2(20),"
-                                + "TELEFONO NUMBER (11))");
+                        System.out.println("\nIntroducir datos de novo cliente\n"
+                                + "----------------------------------\n\n");
+                        try{
+                            System.out.print("Introduza nome: ");
+                            nome = teclado.nextLine();
+                            System.out.print("Introduza teléfono: ");
+                            telefono = Double.parseDouble(teclado.nextLine());
+                            stm=con.createStatement();
+                            rs=stm.executeQuery("SELECT COD_CLIENTE FROM CLIENTES");
+                            while(rs.next()){
+                                codcliente.add(rs.getInt("COD_CLIENTE"));
+                            }
+                            contador =1;
+                            while (codcliente.contains(contador)){
+                                contador ++;
+                            }
+                            stm.executeUpdate("INSERT INTO CLIENTES ("
+                                    + "COD_CLIENTE,NOMBRE,TELEFONO)"
+                                    + "VALUES ("
+                                    + contador+",'"+nome+"',"+telefono+")");
+                        }catch(Exception ex){
+                            System.out.println("Datos introducidos erróneos:\n"
+                                    + ""+ex.getMessage());        
+                        }
+                        codcliente.clear();
+                        JOptionPane.showMessageDialog(null, "Cliente creado","Cliente creado correctamente",JOptionPane.INFORMATION_MESSAGE);
                         break;
                     case 2:
-                        stm=con.createStatement();
-                        stm.executeUpdate("CREATE TABLE ESCRITURAS ("
-                                + "CODIGO VARCHAR2(5) PRIMARY KEY,"
-                                + "TIPO VARCHAR2(4),"
-                                + "NOM_FICH VARCHAR (40),"
-                                + "NUM_INTERV NUMBER (2))");
+                        System.out.println("\nIntroducir datos de nova escritura\n"
+                                + "----------------------------------\n\n");
+                        try{
+                            System.out.print("Introduza tipo de documento (4 caracteres): ");
+                            tipo = teclado.nextLine();
+                            System.out.print("Introduza nome do ficheiro: ");
+                            fich = teclado.nextLine();
+                            System.out.print("Introduza persoas involucradas: ");
+                            perso = Integer.parseInt(teclado.nextLine());
+                            stm=con.createStatement();
+                            rs=stm.executeQuery("SELECT CODIGO FROM ESCRITURAS");
+                            while(rs.next()){
+                                codcliente.add(rs.getInt("CODIGO"));
+                            }
+                            contador =1;
+                            while (codcliente.contains(contador)){
+                                contador ++;
+                            }
+                            stm.executeUpdate("INSERT INTO ESCRITURAS "
+                                    + "VALUES ("
+                                    + contador+",'"+tipo+"','"+fich+"',"+perso+")");
+                        }catch(Exception ex){
+                            System.out.println("Datos introducidos erróneos:\n"
+                                    + ""+ex.getMessage());        
+                        }
+                        codcliente.clear();
+                        JOptionPane.showMessageDialog(null, "Escritura creada","Escritura creada correctamente",JOptionPane.INFORMATION_MESSAGE);
                         break;
-                    case 3:
-                        stm=con.createStatement();
-                        stm.executeUpdate("CREATE TABLE ESCCLI ("
-                                + "COD_CLIENTE NUMBER(5) REFERENCES CLIENTES,"
-                                + "CODIGO VARCHAR2(5) REFERENCES ESCRITURAS)");                   
-                        break; 
                     case 4:
-                        seleccion = JOptionPane.showConfirmDialog(null,"Engadir datos automáticamente (premer OK) ou cancelar", "Introducción de datos", JOptionPane.OK_CANCEL_OPTION);
-                        if (seleccion == 0){
-                            stm=con.createStatement();
-                            stm.executeUpdate("INSERT INTO CLIENTES ("
-                                    + "COD_CLIENTE,NOMBRE,TELEFONO)"
-                                    + "VALUES ("
-                                    + "0001,'Pedro',986254125)");
-                            stm.executeUpdate("INSERT INTO CLIENTES ("
-                                    + "COD_CLIENTE,NOMBRE,TELEFONO)"
-                                    + "VALUES ("
-                                    + "0002,'Paco',662254125)");
-                            stm.executeUpdate("INSERT INTO CLIENTES ("
-                                    + "COD_CLIENTE,NOMBRE,TELEFONO)"
-                                    + "VALUES ("
-                                    + "0003,'Raquel',null)");
-                        }
-                        break;
-                    case 5:
-                        seleccion = JOptionPane.showConfirmDialog(null,"Engadir datos automáticamente (premer OK) ou cancelar", "Introducción de datos", JOptionPane.OK_CANCEL_OPTION);
-                        if (seleccion == 0){
-                            stm=con.createStatement();
-                            stm.executeUpdate("INSERT INTO ESCRITURAS "
-                                    + "VALUES ("
-                                    + "'A001C','TEST','TESTAMENTO_PACO_1.DOC',2)");
-                            stm.executeUpdate("INSERT INTO ESCRITURAS "
-                                    + "VALUES ("
-                                    + "'B078F','TEST','TESTAMENTO_PEDRO_1.DOC',2)");
-                            stm.executeUpdate("INSERT INTO ESCRITURAS "
-                                    + "VALUES ("
-                                    + "'GF7R2','CPVE','COMPRA_PISO_GF7R2_1.DOC',4)");
-                            stm.executeUpdate("INSERT INTO ESCCLI "
-                                    + "VALUES ("
-                                    + "0001,'B078F')");
-                            stm.executeUpdate("INSERT INTO ESCCLI "
-                                    + "VALUES ("
-                                    + "0002,'A001C')");
-                            stm.executeUpdate("INSERT INTO ESCCLI "
-                                    + "VALUES ("
-                                    + "0003,'GF7R2')");
-                        }
-                        break;
-                    case 6:
                         stm=con.createStatement();
                         rs=stm.executeQuery("SELECT * FROM CLIENTES");
                         System.out.println("\nDatos de Clientes:\n"
                                 + "--------------------------------------------\n");
                         while(rs.next()){
-                            System.out.println("Código: "+rs.getString("COD_CLIENTE")+" Nome: "+rs.getString("NOMBRE")+" Teléfono: "+rs.getString("TELEFONO"));
+                            System.out.println("Código: "+rs.getString(1)+" Nome: "+rs.getString(2)+" Teléfono: "+rs.getString(3));
                         }
                         System.out.println("\n");
                         break;
-                    case 7:
+                    case 5:
+                        System.out.println("\nModificar cliente\n"
+                                + "----------------------------------\n\n");
+                        try{
+                            System.out.print("Introduza código: ");
+                            cod_cli = Integer.parseInt(teclado.nextLine());
+                            System.out.print("Introduza nome: ");
+                            nome = teclado.nextLine();
+                            System.out.print("Introduza teléfono: ");
+                            telefono = Double.parseDouble(teclado.nextLine());
+                            stm=con.createStatement();
+                            stm.executeUpdate("UPDATE CLIENTES SET NOMBRE = '"+nome+"' "
+                                    + ",TELEFONO ="+telefono+" WHERE COD_CLIENTE = "+cod_cli+"");
+                            
+                        } catch (Exception ex){
+                            System.out.println("Datos introducidos erróneos:\n"
+                                    + ""+ex.getMessage());
+                        }
+                        JOptionPane.showMessageDialog(null, "Cliente modificado","Cliente modificado correctamente",JOptionPane.INFORMATION_MESSAGE);   
+                        break;
+                    case 6:
                         stm=con.createStatement();
                         rs=stm.executeQuery("SELECT * FROM ESCRITURAS");
                         System.out.println("\nDatos de Escrituras:\n"
@@ -121,29 +143,97 @@ public class Database {
                             System.out.println("Código: "+rs.getString(1)+" Tipo: "+rs.getString(2)+" Ficheiro: "+rs.getString(3)+" Interveñen: "+rs.getString(4));
                         }
                         System.out.println("\n");
+                        break; 
+                    case 7:
+                        System.out.println("\nModificar escritura\n"
+                                + "----------------------------------\n\n");
+                        try{
+                            System.out.print("Introduza código: ");
+                            cod_esc = Integer.parseInt(teclado.nextLine());
+                            System.out.print("Introduza tipo de documento (4 caracteres): ");
+                            tipo = teclado.nextLine();
+                            System.out.print("Introduza nome do ficheiro: ");
+                            fich = teclado.nextLine();
+                            System.out.print("Introduza persoas involucradas: ");
+                            perso = Integer.parseInt(teclado.nextLine());
+                            stm=con.createStatement();
+                            stm.executeUpdate("UPDATE ESCRITURAS SET TIPO = '"+tipo+"' "
+                                    + ",NOM_FICH ='"+fich+"' ,NUM_INTERV = "+perso+" WHERE CODIGO = "+cod_esc+"");
+                            
+                        } catch (Exception ex){
+                            System.out.println("Datos introducidos erróneos:\n"
+                                    + ""+ex.getMessage());
+                        }
+                        JOptionPane.showMessageDialog(null, "Escritura modificada","Escritura modificada correctamente",JOptionPane.INFORMATION_MESSAGE);      
+                        break;    
+                        
+                    case 3:  
+                        System.out.println("\nVincular rexistros\n"
+                                + "----------------------------------\n\n");
+
+                        try{
+                            System.out.print("Introduza código de cliente: ");
+                            cod_cli = Integer.parseInt(teclado.nextLine());
+                            System.out.print("Introduza código de escritura: ");
+                            cod_esc = Integer.parseInt(teclado.nextLine());
+                            stm=con.createStatement();
+                            // búsqueda en clientes para verificar que existe
+                            rs=stm.executeQuery("SELECT COD_CLIENTE FROM CLIENTES");
+                            while(rs.next()){
+                                codcliente.add(rs.getInt("COD_CLIENTE"));
+                            }
+                            if (!codcliente.contains(cod_cli)){
+                                throw new Exception("Código cliente non existe");
+                            }
+                            codcliente.clear();
+                            
+                            //búsqueda en escrituras para verificar que existe
+                            rs=stm.executeQuery("SELECT CODIGO FROM ESCRITURAS");
+                            while(rs.next()){
+                                codcliente.add(rs.getInt("CODIGO"));
+                            }
+                            if (!codcliente.contains(cod_esc)){
+                                throw new Exception("Código escritura non existe");
+                            }
+                            codcliente.clear();
+                            
+                            // Introduzo vínculo
+                            
+                            stm.executeUpdate("INSERT INTO ESCCLI "
+                                    + "VALUES ("
+                                    + cod_cli+","+cod_esc+")");
+                            JOptionPane.showMessageDialog(null, "Vínculo creado correctamente","Vínculo creado",JOptionPane.INFORMATION_MESSAGE);
+
+                            
+                        }catch (Exception ex){
+                            System.out.println("Datos introducidos erróneos:\n"
+                                    + ""+ex.getMessage());
+                        }    
+                            
+                            
+                            
+                            
+                            
+                        codcliente.clear();    
                         break;
                     case 8:
-                        stm=con.createStatement();
-                        stm.executeUpdate("UPDATE CLIENTES SET TELEFONO =675849875 WHERE COD_CLIENTE = 3");
-                        break;
-                    case 9:
+                        System.out.println("\nClientes con escrituras\n"
+                                + "----------------------------------\n\n");
                         stm=con.createStatement();
                         rs=stm.executeQuery("SELECT CLIENTES.NOMBRE FROM CLIENTES, ESCRITURAS, ESCCLI WHERE ("
                                 + "CLIENTES.COD_CLIENTE = ESCCLI.COD_CLIENTE AND "
                                 + "ESCRITURAS.CODIGO = ESCCLI.CODIGO AND "
-                                + "ESCRITURAS.TIPO='CPVE')");
-                        System.out.println("\nDatos de clientes con documento tipo CPVE:\n"
-                                + "--------------------------------------------\n");
+                                + "ESCRITURAS.TIPO IS NOT NULL)");
                         while(rs.next()){
                             System.out.println("Nome: "+rs.getString(1));
                         }
                         System.out.println("\n");
-                        break;
-                    case 10:
+                        break;    
+                    case 9:
                         System.out.println("Saíndo do programa");
                         con.close();
                         flag = true;
-                        break;
+                        break;                     
                 }
             }
             
@@ -153,14 +243,6 @@ public class Database {
         {
             if (ex.getMessage().contains("ORA-01017")){
                 JOptionPane.showMessageDialog(null, "Usuario de base de datos erróneo");
-            } else if (ex.getMessage().contains("ORA-00955")){
-                JOptionPane.showMessageDialog(null, "Tabla ya existe en la base de datos");
-            } else if (ex.getMessage().contains("ORA-00942")){
-                JOptionPane.showMessageDialog(null, "Falta tabla de referencia en la base de datos");
-            } else if (ex.getMessage().contains("PROG.SYS_C004728")){
-                JOptionPane.showMessageDialog(null, "Clave primaria duplicada (¿os datos xa estaban creados?)");
-            } else if (ex.getMessage().contains("ORA-00001")){
-                JOptionPane.showMessageDialog(null, "Restricción única violada (¿os datos xa estaban creados?)");
             } else{
                 System.out.println(ex.getMessage());
                 ex.printStackTrace();
@@ -173,18 +255,17 @@ public class Database {
     public static int Menu(){
         String retorno;
         int valor = 0;
-        while (valor <1 || valor >10){
+        while (valor <1 || valor >9){
             retorno = JOptionPane.showInputDialog(null,"Selecciona unha opción:\n\n"
-                    + "1- Crear tabla Clientes\n"
-                    + "2- Crear tabla Escrituras\n"
-                    + "3- Crear tabla EscCli\n"
-                    + "4- Insertar datos en Clientes\n"
-                    + "5- Insertar datos en Escrituras y EscCli\n"
-                    + "6- Recuperar datos de tabla Clientes\n"
-                    + "7- Recuperar datos de tabla Escritura\n"
-                    + "8- Actualizar tabla Clientes\n"
-                    + "9- Listar nombre de clientes con tipo escritura CPVE \n"
-                    + "10- Sair \n\n");
+                    + "1- Dar de alta clientes\n"
+                    + "2- Crear escrituras\n"
+                    + "3- Vincular cliente e escritura\n"                    
+                    + "4- Consultar clientes\n"
+                    + "5- Modificar clientes\n"                    
+                    + "6- Consultar escrituras\n"
+                    + "7- Modificar escrituras\n"      
+                    + "8- Listar os clientes con escrituras\n"
+                    + "9- Sair \n\n");
             try{
                 valor = Integer.parseInt(retorno);
             }catch (Exception ex){
